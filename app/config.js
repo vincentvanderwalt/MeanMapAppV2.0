d48b7ca7758d120f413f12c0c68d2d0ead1a75b2
vincent van der walt <vincent.vanderwalt@zonal.co.uk>
2019-12-05 08:56:19 +0000

    update packages and mongo details

--- app/config.js
+++ app/config.js
@@ -2,25 +2,11 @@
 
 module.exports = {
 
-    mongolab:
+    bitnami:
     {
-        name: "mongolab",
-        url: "mongodb://BigCoder:BigCoder!@ds139817.mlab.com:39817/heroku_cnkn5vpn",
-        port: 27017
-    },
-
-    local:
-    {
-        name: "scotch-user-map-local",
-        url: "mongodb://localhost/MeanMapApp",
-        port: 27017
-    },
-
-    localtest:
-    {
-        name: "scotch-user-map-local",
-        url: "mongodb://localhost/MeanMapAppTest",
-        port: 27017
+        name: "MongoDB Service",
+        url: "mongodb://" + process.env.DATABASE_USERNAME + ":" + process.env.DATABASE_PASSWORD + "@" + process.env.DATABASE_HOST + "/" + process.env.DATABASE_NAME,
+        port: process.env.DATABASE_PORT
     }
 
 };
